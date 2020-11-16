9bc39ceeba8da20c7e928459b0b3dc605e74ec30
Edpacca <eddie@pace.me.uk>
2020-11-16 15:13:46 +0000

    Added classes from command line program to XAML program. Implemented Name dropdown

--- ManticoreViewer/MainWindow.xaml.cs
+++ ManticoreViewer/MainWindow.xaml.cs
@@ -15,14 +15,40 @@ using System.Windows.Shapes;
 
 namespace ManticoreViewer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private string _databaseFilePath = @"C:\Users\eddie\OneDrive\ComputerScience\ProjectManticore\Database\5e_monsters.json";
+        private StatBlockParser _parser = new StatBlockParser();
+        private dynamic _monsters;
+        private List<Stats> _monsterStats;
+
         public MainWindow()
         {
             InitializeComponent();
+            _monsters = Deserialiser.DeserialiseJSON(_databaseFilePath);
+            _monsterStats = _parser.ParseObjects(_monsters);
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                nameComboBox.DisplayMemberPath = "Name";
+                nameComboBox.ItemsSource = _monsterStats;
+                nameComboBox.Text = "Select monster";
+                //STR.Text = nameComboBox.SelectedValuePath = "STR";
+                //DEX.Text = nameComboBox.SelectedValuePath = "DEX";
+                //CON.Text = nameComboBox.SelectedValuePath = "CON";
+                //INT.Text = nameComboBox.SelectedValuePath = "INT";
+                //WIS.Text = nameComboBox.SelectedValuePath = "WIS";
+                //CHA.Text = nameComboBox.SelectedValuePath = "CHA";
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error occured");
+            }
         }
     }
 }
---
+++ ManticoreViewer/StatBlockParser.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ManticoreViewer
+{
+    public class StatBlockParser
+    {
+        public List<Stats> ParseObjects(dynamic unparsedObjects)
+        {
+            List<Stats> parsedObjects = new List<Stats>();
+            foreach (var unparsedObject in unparsedObjects)
+            {
+                parsedObjects.Add(ParseObject(unparsedObject));
+            }
+
+            return parsedObjects;
+        }
+
+        private Stats ParseObject(dynamic unparsedObject)
+        {
+            Stats parsedStats = new Stats();
+            parsedStats.Name = unparsedObject.name;
+            parsedStats.Strength = (byte)unparsedObject.STR;
+            parsedStats.Dexterity = (byte)unparsedObject.DEX;
+            parsedStats.Constitution = (byte)unparsedObject.CON;
+            parsedStats.Intelligence = (byte)unparsedObject.INT;
+            parsedStats.Wisdom = (byte)unparsedObject.WIS;
+            parsedStats.Charisma = (byte)unparsedObject.CHA;
+            parsedStats.ChallengeRating = ParseChallengeRating(unparsedObject.Challenge);
+            parsedStats.ArmourClass = ParseAC(unparsedObject.ArmorClass);
+            parsedStats.AvgHitPoints = ParseHP(unparsedObject.HitPoints);
+
+            return parsedStats;
+        }
+
+        private float ParseChallengeRating(dynamic unparsed)
+        {
+            string unparsedString = unparsed.ToString();
+            string challengeRatingString = unparsedString.Substring(0, unparsedString.IndexOf(' '));
+            float challengeRating;
+
+            if (challengeRatingString.Contains("/"))
+            {
+                string[] CR = challengeRatingString.Split('/');
+                challengeRating = (float)(Convert.ToDouble(CR[0]) / Convert.ToDouble(CR[1]));
+            }
+            else
+                challengeRating = (float)(Convert.ToInt16(challengeRatingString));
+
+            return challengeRating;
+        }
+
+        private byte ParseAC(dynamic unparsed)
+        {
+            string unparsedString = unparsed.ToString();
+            try
+            {
+                byte AC = Convert.ToByte(unparsedString);
+                return AC;
+            }
+            catch (Exception)
+            {
+                string ACString = unparsedString.Substring(0, unparsedString.IndexOf(' '));
+                return Convert.ToByte(ACString);
+            }
+        }
+
+        private int ParseHP(dynamic unparsed)
+        {
+            string unparsedString = unparsed.ToString();
+            string HPString = unparsedString.Substring(0, unparsedString.IndexOf(' '));
+            return Convert.ToInt32(HPString);
+        }
+
+    }
+}
