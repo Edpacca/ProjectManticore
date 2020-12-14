74b492e238dcfdd49737b317ada9075f113bc519
Edpacca <eddie@pace.me.uk>
2020-12-14 17:50:17 +0000

    Updated stats to calculate ability modifiers

--- ManticoreViewer/Stats.cs
+++ ManticoreViewer/Stats.cs
@@ -1,25 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace ManticoreViewer
 {
     public class Stats
     {
         public string Name { get; set; }
-        public byte Strength { get; set; }
-        public byte Dexterity { get; set; }
-        public byte Constitution { get; set; }
-        public byte Intelligence { get; set; }
-        public byte Wisdom { get; set; }
-        public byte Charisma { get; set; }
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
         public float ChallengeRating { get; set; }
-        public byte ArmourClass { get; set; }
+        public int ArmourClass { get; set; }
         public int AvgHitPoints { get; set; }
         public string Speed { get; set; }
 
         public string ImgURL { get; set; }
+        public string Actions { get; set; }
+
+        public List<string> Modifiers { get; private set; }
+
+        public void SetModifiers()
+        {
+            Modifiers = ParseModifiers();
+        }
+
+        private List<string> ParseModifiers()
+        {
+            List<string> modifiers = new List<string>();
+
+            modifiers.Add(Utility.StringModifier(Strength));
+            modifiers.Add(Utility.StringModifier(Dexterity));
+            modifiers.Add(Utility.StringModifier(Constitution));
+            modifiers.Add(Utility.StringModifier(Intelligence));
+            modifiers.Add(Utility.StringModifier(Wisdom));
+            modifiers.Add(Utility.StringModifier(Charisma));
+
+            return modifiers;
+        }
     }
 }
