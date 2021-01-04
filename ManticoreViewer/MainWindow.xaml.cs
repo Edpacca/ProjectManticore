ef1ba432b03b34be76032b8501afb6bc5df5a980
Edpacca <eddie@pace.me.uk>
2021-01-04 12:06:31 +0000

    set file path

--- ManticoreViewer/MainWindow.xaml.cs
+++ ManticoreViewer/MainWindow.xaml.cs
@@ -1,24 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ManticoreViewer
 {
     public partial class MainWindow : Window
     {
-        //private string _databaseFilePath = @"C:\Users\eddie\OneDrive\ComputerScience\ProjectManticore\Database\5e_monsters.json";
-        private string _databaseFilePath = @"C:\Users\eddie\Documents\5e_monsters.json";
+        private string _databaseFilePath = @"C:\Users\eddie\OneDrive\ComputerScience\ProjectManticore\Database\5e_monsters.json";
+        //private string _databaseFilePath = @"C:\Users\eddie\Documents\5e_monsters.json";
         private StatBlockParser _parser = new StatBlockParser();
         private dynamic _monsters;
         private List<Stats> _monsterStats;
@@ -27,7 +16,7 @@ namespace ManticoreViewer
         {
             InitializeComponent();
             _monsters = Deserialiser.DeserialiseJSON(_databaseFilePath);
-            _monsterStats = _parser.ParseObjects(_monsters);
+            _monsterStats = _parser.ParseObjects(_monsters) ?? new List<Stats>();
 
             Loaded += MainWindow_Loaded;
         }
