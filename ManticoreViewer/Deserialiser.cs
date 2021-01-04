6aafc7cba0817f9d74aed49bca51e4be90d5affa
Edpacca <eddie@pace.me.uk>
2021-01-04 12:14:32 +0000

    improved exception handling

--- ManticoreViewer/Deserialiser.cs
+++ ManticoreViewer/Deserialiser.cs
@@ -11,25 +11,21 @@ namespace ManticoreViewer
 
         public static List<object> DeserialiseJSON(string filePath)
         {
-            using (StreamReader reader = File.OpenText(filePath))
-            {
                 try
                 {
-                    string json = reader.ReadToEnd();
-                    List<object> objects = JsonConvert.DeserializeObject<List<object>>(json);
-                    return objects;
+                    using (StreamReader reader = File.OpenText(filePath))
+                    {
+                        string json = reader.ReadToEnd();
+                        List<object> objects = JsonConvert.DeserializeObject<List<object>>(json);
+                        return objects;
+                    }
                 }
                 catch (Exception)
                 {
                     Console.WriteLine("unexpected error reading file");
                 }
-                finally
-                {
-                    reader.Dispose();
-                }
 
                 return null;
-            }
         }
 
         internal static List<object> DeserialiseJSON(object databaseFilePath)
