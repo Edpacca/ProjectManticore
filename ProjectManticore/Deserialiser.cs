526987a600735b7f6489987e8b9ecc7a4165cd46
Edpacca <eddie@pace.me.uk>
2021-01-06 16:53:22 +0000

    Test for pulling databse from website asycnhronously

--- ProjectManticore/Deserialiser.cs
+++ ProjectManticore/Deserialiser.cs
@@ -1,28 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace ProjectManticore
 {
     public class Deserialiser
     {
-        private StreamReader reader;
-
-        public static List<object> DeserialiseJSON(string filePath)
+        public static List<object> DeserialiseJSONFromDisk(string filePath)
         {
-            using (StreamReader reader = File.OpenText(filePath))
+            try
             {
-                string json = reader.ReadToEnd();
-                List<object> objects = JsonConvert.DeserializeObject<List<object>>(json);
-                return objects;
+                using (StreamReader reader = File.OpenText(filePath))
+                {
+                    string json = reader.ReadToEnd();
+                    List<object> objects = JsonConvert.DeserializeObject<List<object>>(json);
+                    return objects;
+                }
             }
+            catch (Exception)
+            {
+                Console.WriteLine("unexpected error reading file");
+            }
+
+            return null;
         }
 
-        internal static List<object> DeserialiseJSON(object databaseFilePath)
+        public static void DeserialiseJSONFromWeb(string WebPath)
         {
-            throw new NotImplementedException();
+            var content = JSONFromWebAsync(WebPath);
+            Console.WriteLine(content.Result);
+            //List<object> objects = JsonConvert.DeserializeObject<List<object>>(content.Result);
+
+            //return objects;
+        }
+
+        public static async Task<string> JSONFromWebAsync(string WebPath)
+        {
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                var dataBase = await client.GetStringAsync(WebPath);
+
+                return dataBase;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
     }
 }
