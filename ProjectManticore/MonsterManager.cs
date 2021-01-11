095289859c12a99c77fe6bee95500a095da70844
Edpacca <eddie@pace.me.uk>
2021-01-11 16:15:15 +0000

    Modified to take interface in constructor

--- ProjectManticore/MonsterManager.cs
+++ ProjectManticore/MonsterManager.cs
@@ -1,46 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace ProjectManticore
 {
     public class MonsterManager
     {
-        private string _databaseFilePath = @"C:\Users\eddie\OneDrive\ComputerScience\ProjectManticore\Database\5e_monsters.json";
-        private string _databaseHTML = "https://gist.githubusercontent.com/tkfu/9819e4ac6d529e225e9fc58b358c3479/raw/d4df8804c25a662efc42936db60cfbc0a5b19db8/srd_5e_monsters.json";
-
-        private dynamic _monsters;
         private StatBlockParser _parser = new StatBlockParser();
-        public List<Stats> MonsterStats { get => _monsterStats; }
-        private List<Stats> _monsterStats = new List<Stats>();
 
-        public MonsterManager()
+        public List<Stats> MonsterStats { get; private set; }
+        public List<object> MonsterList { get; private set; }
+
+        public MonsterManager(IFileDeserialiser deserialiser, string path)
         {
-            _monsters = Deserialiser.DeserialiseJSONFromDisk(_databaseFilePath);
-            _monsterStats = _parser.ParseObjects(_monsters);
-
-            Deserialiser.DeserialiseJSONFromWeb(_databaseHTML);
-        }
-
-        public void PrintMonsters()
-        {
-            foreach (var monster in _monsters)
-            {
-                Console.WriteLine(monster);
-            }
-        }
-
-        public void PrintFirstMonster()
-        {
-            Console.WriteLine(_monsters[0]);
-        }
-
-        public void PrintMonsterNames()
-        {
-            foreach (var name in _parser.ParseObjects(_monsters))
-            {
-                Console.WriteLine(name);
-            }
+            MonsterStats = _parser.ParseObjects(deserialiser.Deserialise(path));
         }
     }
 }
