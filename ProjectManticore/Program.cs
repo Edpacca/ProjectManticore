859ee70d44253dc02b3ad8e46f2e1d9328708ceb
Edpacca <eddie@pace.me.uk>
2021-01-11 16:15:33 +0000

    Testing new interfaces from main

--- ProjectManticore/Program.cs
+++ ProjectManticore/Program.cs
@@ -7,15 +7,20 @@ namespace ProjectManticore
     {
         static void Main(string[] args)
         {
-            PrintMonstersTest();
-        }
-        
-        static void PrintMonstersTest()
-        {
-            MonsterManager _monsterManager = new MonsterManager();
-            //_monsterManager.PrintMonsters();
-            //_monsterManager.PrintFirstMonster();
-            //_monsterManager.PrintMonsterNames();
+            string databaseDiskPath = @"C:\Users\eddie\OneDrive\ComputerScience\ProjectManticore\Database\5e_monsters.json";
+            string databaseWebPath = "https://gist.githubusercontent.com/tkfu/9819e4ac6d529e225e9fc58b358c3479/raw/d4df8804c25a662efc42936db60cfbc0a5b19db8/srd_5e_monsters.json";
+
+            IFileDeserialiser webDeserialiser = new WebDeserialiser();
+            IFileDeserialiser diskDeserialiser = new DiskDeserialiser();
+
+            MonsterManager monsterManager = new MonsterManager(diskDeserialiser, databaseDiskPath);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(monsterManager.MonsterStats[i].Dexterity);
+
+                Console.WriteLine("");
+            }
         }
     }
 }
