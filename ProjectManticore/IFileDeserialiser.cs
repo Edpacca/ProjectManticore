6e51c81e5b708fc6afc86c59883513f2cdc63220
Edpacca <eddie@pace.me.uk>
2021-01-11 16:14:05 +0000

    Implementing new interface IFileDeserialiser for breaking down JSON objects from a file path

---
+++ ProjectManticore/IFileDeserialiser.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ProjectManticore
+{
+    public interface IFileDeserialiser
+    {
+        List<object> Deserialise(string filePath);
+    }
+}
---
+++ ProjectManticore/WebDeserialiser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace ProjectManticore
+{
+    public class WebDeserialiser : IFileDeserialiser
+    {
+        List<object> IFileDeserialiser.Deserialise(string filePath)
+        {
+            return JsonConvert.DeserializeObject<List<object>>(StringFromWeb(filePath));
+        }
+
+        // Make asynchronous
+        public static string StringFromWeb(string WebPath)
+        {
+            return WebStringScraperAsync(WebPath).Result;
+        }
+
+        private static async Task<string> WebStringScraperAsync(string WebPath)
+        {
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                return await client.GetStringAsync(WebPath);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
