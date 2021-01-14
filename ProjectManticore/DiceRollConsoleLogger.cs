5254f32770ff3cab55ac39b6c55190bca0b49567
Edpacca <eddie@pace.me.uk>
2021-01-14 11:52:15 +0000

    Interface and concrete implementation for logging the dice result string and value

---
+++ ProjectManticore/DiceRollConsoleLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManticore
+{
+    class DiceRollConsoleLogger : ILogger
+    {
+        public void LogString(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void LogValue(int value)
+        {
+            Console.WriteLine("Total: " + value);
+        }
+    }
+}
---
+++ ProjectManticore/ILogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManticore
+{
+    public interface ILogger
+    {
+        void LogString(string message);
+        void LogValue(int value);
+    }
+}
