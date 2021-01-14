e31c1b925f53376fbf61a19d88380a7b59ff422e
Edpacca <eddie@pace.me.uk>
2021-01-14 11:49:45 +0000

    created struct to represent a single dice roll

---
+++ ProjectManticore/Dice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManticore
+{
+    public struct Dice
+    {
+        public int Number { get; private set; }
+        public int Type { get; private set; }
+        public int Modifier { get; set; }
+
+        public Dice(int type)
+        {
+            Number = 1;
+            Type = type;
+            Modifier = 0;
+        }
+
+        public Dice(int number, int type)
+        {
+            Number = number;
+            Type = type;
+            Modifier = 0;
+        }
+
+        public Dice(int number, int type, int modifier)
+        {
+            Number = number;
+            Type = type;
+            Modifier = modifier;
+        }
+    }
+}
