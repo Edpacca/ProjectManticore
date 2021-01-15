00b304cab41a9f43c05e08d33855f281eb32b3d2
Edpacca <eddie@pace.me.uk>
2021-01-15 16:28:54 +0000

    changed layout, added dice roll button

--- ManticoreViewer/MainWindow.xaml.cs
+++ ManticoreViewer/MainWindow.xaml.cs
@@ -11,14 +11,16 @@ namespace ManticoreViewer
 
         MonsterManager monsterManager;
         List<Stats> _monsterStats;
+        DiceRoller diceRoller = new DiceRoller();
 
         public MainWindow()
         {
             InitializeComponent();
             monsterManager = new MonsterManager(diskDeserialiser, databaseDiskPath);
-
+            
             _monsterStats = monsterManager.MonsterStats ?? new List<Stats>();
             Loaded += MainWindow_Loaded;
+            //rollButton.Click += diceRoller.OnDiceRolled;
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
@@ -32,6 +34,33 @@ namespace ManticoreViewer
             catch (Exception)
             {
                 Console.WriteLine("Error occured");
+            }
+        }
+
+        private void AddButtonClick(object sender, EventArgs e)
+        {
+
+        }
+
+        public delegate void DiceRolledEventHandler(object source, RoutedEventArgs args);
+        public event DiceRolledEventHandler DiceRolled;
+
+        protected virtual void OnDiceRolled(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int numberOfDice = Convert.ToInt32(diceNumber.Text);
+                int typeOfDice = Convert.ToInt32(diceType.Text);
+                int modifier = Convert.ToInt32(diceModifier.Text);
+
+                diceRoller.OnDiceRolled(sender, new DiceEventArgs() { Dice = new Dice(numberOfDice, typeOfDice, modifier) });
+
+               //if (DiceRolled != null)
+                    //DiceRolled(sender, new DiceEventArgs() { Dice = new Dice(numberOfDice, typeOfDice, modifier) });
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Dice parameters must be integers");
             }
         }
     }
