da79e8ac6dd28e10f863a3693f1c2e2cc88930f3
Edpacca <eddie@pace.me.uk>
2021-01-15 13:42:52 +0000

    updated main project and window file for structural rework

--- ManticoreViewer/MainWindow.xaml.cs
+++ ManticoreViewer/MainWindow.xaml.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
+using ProjectManticore;
 
 namespace ManticoreViewer
 {
     public partial class MainWindow : Window
     {
-        private string _databaseFilePath = @"C:\Users\eddie\OneDrive\ComputerScience\ProjectManticore\Database\5e_monsters.json";
-        //private string _databaseFilePath = @"C:\Users\eddie\Documents\5e_monsters.json";
-        private StatBlockParser _parser = new StatBlockParser();
-        private dynamic _monsters;
-        private List<Stats> _monsterStats;
+        string databaseDiskPath = @"C:\Users\eddie\OneDrive\ComputerScience\ProjectManticore\Database\5e_monsters.json";
+        //string databaseWebPath = "https://gist.githubusercontent.com/tkfu/9819e4ac6d529e225e9fc58b358c3479/raw/d4df8804c25a662efc42936db60cfbc0a5b19db8/srd_5e_monsters.json";
+
+        //IFileDeserialiser webDeserialiser = new WebDeserialiser();
+        IFileDeserialiser diskDeserialiser = new DiskDeserialiser();
+
+        MonsterManager monsterManager;
+        List<Stats> _monsterStats;
 
         public MainWindow()
         {
             InitializeComponent();
-            _monsters = Deserialiser.DeserialiseJSON(_databaseFilePath);
-            _monsterStats = _parser.ParseObjects(_monsters) ?? new List<Stats>();
+            monsterManager = new MonsterManager(diskDeserialiser, databaseDiskPath);
 
+            _monsterStats = monsterManager.MonsterStats ?? new List<Stats>();
             Loaded += MainWindow_Loaded;
         }
 
