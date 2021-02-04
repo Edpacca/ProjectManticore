a5800e598b4de3b26b3e00a1bdb65289e423cb93
Edpacca <eddie@pace.me.uk>
2021-02-04 13:26:17 +0000

    changed rollbutton method to show string and int

--- ManticoreViewer/MainWindow.xaml.cs
+++ ManticoreViewer/MainWindow.xaml.cs
@@ -8,12 +8,15 @@ namespace ManticoreViewer
     public partial class MainWindow : Window
     {
         string databaseDiskPath = @"C:\Users\eddie\OneDrive\ComputerScience\ProjectManticore\Database\5e_monsters.json";
+        //string databaseDiskPath = @"C:\Users\eddie\Documents\OneDrive\ComputerScience\ProjectManticore\Database\5e_monsters.json";
         IFileDeserialiser diskDeserialiser = new DiskDeserialiser();
 
         MonsterManager _monsterManager;
         List<Monster> _monsterDatabase;
         public ObservableCollection<Monster> activeMonsters;
         DiceRoller diceRoller = new DiceRoller();
+
+        int rollResult;
 
         public MainWindow()
         {
@@ -57,6 +60,12 @@ namespace ManticoreViewer
             }
         }
 
+        private void RemoveButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (activeMonsters.Count > 0)
+                activeMonsters.RemoveAt(activeMonsters.Count - 1);
+        }
+
         protected virtual void OnDiceRolled(object sender, RoutedEventArgs e)
         {
             try
@@ -65,12 +74,25 @@ namespace ManticoreViewer
                 int typeOfDice = Convert.ToInt32(diceType.Text);
                 int modifier = Convert.ToInt32(diceModifier.Text);
 
-                diceRoller.OnDiceRolled(sender, new DiceEventArgs() { Dice = new Dice(numberOfDice, typeOfDice, modifier) });
+                DRollResult roll = diceRoller.OnDiceRolled(sender, new DiceEventArgs() { Dice = new Dice(numberOfDice, typeOfDice, modifier) });
+                rollResultBox.Text = roll.RollResult.ToString();
+                rollResultStringBox.Text = roll.RollString;
+
             }
             catch (Exception)
             {
                 MessageBox.Show("Dice parameters must be integers");
             }
+        }
+
+        private void SaveButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Saved");
+        }
+
+        private void LoadButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Loaded");
         }
     }
 }
