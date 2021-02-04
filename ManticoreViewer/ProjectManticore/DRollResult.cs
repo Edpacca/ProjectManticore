5dad1547a2018b4731fbc82eed1e3aa2387ccaaa
Edpacca <eddie@pace.me.uk>
2021-02-04 13:26:34 +0000

    created struct to hold die result

---
+++ ManticoreViewer/ProjectManticore/DRollResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManticoreViewer
+{
+    public struct DRollResult
+    {
+        public string RollString;
+        public int RollResult;
+    }
+}
