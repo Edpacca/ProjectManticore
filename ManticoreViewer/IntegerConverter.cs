275abb7d8b3dc153fe73ea1fda25c1b97c85a4bc
Edpacca <eddie@pace.me.uk>
2021-02-05 12:18:54 +0000

    StaticResource for checking HP percentage

---
+++ ManticoreViewer/IntegerConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ManticoreViewer
+{
+    public class IntegerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                int convertedValue = System.Convert.ToInt32(value);
+                return value;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
