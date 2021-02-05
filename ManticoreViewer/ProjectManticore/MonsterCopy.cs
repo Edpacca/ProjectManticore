cadc176a5e9e899dd0dbb7b6c574d83d68343e63
Edpacca <eddie@pace.me.uk>
2021-02-05 12:20:24 +0000

    class for copying monster instance

---
+++ ManticoreViewer/ProjectManticore/MonsterCopy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManticoreViewer
+{
+    public class MonsterCopy
+    {
+        public static Monster CopyMonster(Monster monster)
+        {
+            var copiedMonster = new Monster();
+
+            copiedMonster.Name = monster.Name;
+            copiedMonster.HitPoints = monster.HitPoints;
+            copiedMonster.CurrentHitPoints = monster.CurrentHitPoints;
+            copiedMonster.Strength = monster.Strength;
+            copiedMonster.Dexterity = monster.Dexterity;
+            copiedMonster.Constitution = monster.Constitution;
+            copiedMonster.Intelligence = monster.Intelligence;
+            copiedMonster.Wisdom = monster.Wisdom;
+            copiedMonster.Charisma = monster.Charisma;
+            copiedMonster.ImgURL = monster.ImgURL;
+            copiedMonster.Speed = monster.Speed;
+            copiedMonster.ChallengeRating = monster.ChallengeRating;
+            copiedMonster.ArmourClass = monster.ArmourClass;
+            copiedMonster.SetModifiers();
+
+            return copiedMonster;
+        }
+
+    }
+}
